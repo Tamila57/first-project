a7b506c7dc6c0b7aa8c6eb3c2ac879b0ad3d2bd5
Tamila57 <156967484+Tamila57@users.noreply.github.com>
2024-03-05 16:23:24 +0000

    add js for sendidng form to email

--- js/footer.js
+++ js/footer.js
@@ -7,14 +7,27 @@ async function handleSubmit(event) {
     method: form.method,
     body: new FormData(event.target),
     headers: {
-      Accept: "application/json"
-    }
+      Accept: "application/json",
+    },
   })
-  if (response.ok) {
-    status.innerHTML = "Thanks for your register!";
-    form.reset();
-  } else {
-    status.innerText = "Error!";
-  }
+    .then((response) => {
+      if (response.ok) {
+        status.innerHTML = "Thanks for your submission!";
+        form.reset();
+      } else {
+        response.json().then((data) => {
+          if (Object.hasOwn(data, "errors")) {
+            status.innerHTML = data["errors"]
+              .map((error) => error["message"])
+              .join(", ");
+          } else {
+            status.innerHTML = "Oops! There was a problem submitting your form";
+          }
+        });
+      }
+    })
+    .catch((error) => {
+      status.innerHTML = "Oops! There was a problem submitting your form";
+    });
 }
 form.addEventListener("submit", handleSubmit);
